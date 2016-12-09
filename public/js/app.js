16c42181fd530f8aff603db4bc640578f7129be9
emzeraus <ellamae_1212@yahoo.com>
2016-12-09 18:52:27 +0800

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -272,7 +272,6 @@
 						<div class="input-field">				
 							<input onkeyup="component.bookListItems(this.value)" id="search" type="search" placeholder="Search" required>
 							<label for="search"><i class="material-icons">search</i></label>
-							<i class="material-icons">close</i>
 						</div>
 					</form>
 					</div>
