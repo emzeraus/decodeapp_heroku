a0e9584e058f5cf6f9ec75c98745b28fd08ed778
emzeraus <ellamae_1212@yahoo.com>
2016-12-09 18:56:11 +0800

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -270,7 +270,9 @@
 		    		<div class="nav-wrapper white">
 					<form>
 						<div class="input-field">				
-							<input onkeyup="component.bookListItems(this.value)" id="search" type="search" placeholder="Search" required>
+								<input onkeyup="component.bookListItems(this.value)" id="search" type="search" placeholder="Search" required>
+								<label for="search"><i class="material-icons">search</i></label>
+								<i class="material-icons">close</i>
 						</div>
 					</form>
 					</div>
